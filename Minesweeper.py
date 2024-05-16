afb7d715218472d1f4f0c6a0d51d41fa4be7576f
DANYLOPC\goawr <Goawrh1av@gmail.com>
2024-05-16 11:44:33 +0200

    fixed string to dict conversion

--- Minesweeper.py
+++ Minesweeper.py
@@ -115,7 +115,7 @@ def SetGameInfo(gameinfo):
 def GetMinesweeperInfo():
        gameinfo = GetGameInfo()
        if(gameinfo):
-        minesweeper_info = gameinfo["minesweeper"]
+        minesweeper_info = json.loads(gameinfo)["minesweeper"]
         return minesweeper_info
        else:
            return {}
